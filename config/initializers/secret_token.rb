24a139080f9805caf8d0f43243e2fd65784e8064
Haldun Bayhantopcu <hb@hb.gen.tr>
2011-12-09 18:58:25 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traq::Application.config.secret_token = '10c41f4ff4bb365cb5715607081ac0a1064316819dd5a95aa6d3ef8f1934c38934b46301fa0723831e5a46645cd366256a5fa76638850b123306d5e296793b85'
